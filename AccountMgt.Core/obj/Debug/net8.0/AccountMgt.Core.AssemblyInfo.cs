b4cecf52e6186cd4326944e7ee481d0c7b6dbdac
Lucky Otono <51697000+otono-lucky@users.noreply.github.com>
2024-04-07 17:32:09 +0100

    Otono lucky/resolve (#8)
    * commit changes
    
    * commit changes

--- AccountMgt.Core/obj/Debug/net8.0/AccountMgt.Core.AssemblyInfo.cs
+++ AccountMgt.Core/obj/Debug/net8.0/AccountMgt.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountMgt.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b87e40f4dc9fe3514d04cacee95142cab7bdcb05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f5161d86bc7a78ae182ba781a223eca709566b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountMgt.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountMgt.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
