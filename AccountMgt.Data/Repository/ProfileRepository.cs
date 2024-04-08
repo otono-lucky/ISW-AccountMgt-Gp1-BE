11cc0eb99f3f7ccd0b018b633c36768496843fd8
unknown <izuagbalachere@gmail.com>
2024-04-08 18:21:30 +0100

    creation of the get all profile

--- AccountMgt.Data/Repository/ProfileRepository.cs
+++ AccountMgt.Data/Repository/ProfileRepository.cs
@@ -9,5 +9,22 @@ namespace AccountMgt.Data.Repository
 {
     public class ProfileRepository : IProfileRepository
     {
+        private readonly AppDbContext _context;
+
+        private readonly ProfileManager<AppProfile> _profileManager;
+
+        public ProfileRepository(ProfileManager<AppProfile> profileManager, AppDbContext context)
+        {
+            _profileManager = profileManager;
+            _context = context;
+        }
+
+        public async Task<string> GetAllProfile(GetAllProfileDto request)
+        {
+            var profiles = await _profileManager.GetAllProfileAsync(userId);
+            if (profiles == null)
+            throw new EntityNotFoundException(ids);
+            return profiles;
+        }
     }
 }
