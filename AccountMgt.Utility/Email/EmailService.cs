fa9df8126ccc6dda6f96ace25c093eb5b7d168fe
Lucky Otono <otonolucky4@gmail.com>
2024-05-11 14:49:49 +0100

    emailservice1

--- AccountMgt.Utility/Email/EmailService.cs
+++ AccountMgt.Utility/Email/EmailService.cs
@@ -49,7 +49,7 @@ namespace AccountMgt.Utility.Email
         private string PopulateRegisterEmail(string UserName, string Otp)
         {
             string body = string.Empty;
-            string filePath = Directory.GetCurrentDirectory() + @"\Templates\RegistrationTemplate.html";
+            string filePath = "/app/Templates/RegistrationTemplate.html";
 
             using (StreamReader reader = new StreamReader(filePath))
             {
